627718f97b9d730fc8fec50e0ab0d97059c292b8
Nicolas Corbett <nicolas.corbett@student.ehb.be>
2024-07-21 21:54:10 +0200

    last batch

--- nicolas-corbett/src/index.js
+++ nicolas-corbett/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './routes/App';
 import reportWebVitals from './reportWebVitals';
+
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Ref from './routes/Ref';
+
+const router = createBrowserRouter([
+  {path: '/', 
+   element: <App /> 
+  },{
+    path: '/references',
+    element: <Ref/>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
